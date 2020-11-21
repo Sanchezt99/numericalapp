a1ed7b71ad5f220f2687f1653b33db659b858d3d
Sanchezt99 <msanchezt@eafit.edu.co>
2020-11-21 18:02:50 -0500

    Factorization

---
+++ src/numericalapp/Factorization/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FactorizationConfig(AppConfig):
+    name = 'Factorization'
--- src/numericalapp/numericalapp/urls.py
+++ src/numericalapp/numericalapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from pages.views import home_view, methods_view, about_view
+from Equation_Systems.views import doolittle
 
 urlpatterns = [
     path("", home_view, name = "index.index"),
     path("methods/", methods_view, name="methods.index"),
     path("about/", about_view, name="about.index"),
+    path("doolittle/", doolittle, name='dolittle.index')
 ]
