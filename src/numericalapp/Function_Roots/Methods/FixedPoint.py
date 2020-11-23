b6ff9260c8ef2e540aa3eb1508af3b71e1b639a0
nroldanr <nroldanr@eafit.edu.co>
2020-11-23 01:20:27 -0500

    first method done

--- src/numericalapp/Function_Roots/Methods/FixedPoint.py
+++ src/numericalapp/Function_Roots/Methods/FixedPoint.py
@@ -1,17 +1,23 @@
-import math
+import math, os, sys
+import sympy as sp
+from sympy.parsing.sympy_parser import parse_expr as pe
 from scitools.StringFunction import StringFunction
 import utils.Functions as fc
+
 
 class FixedPoint:
 
     def __init__(self):
         self.values = []
 
-    def evaluate(self, x0, tolerancia, iter, function, g_function, type_error=0):
+    def evaluate(self, x0, tol, iter, f, g, type_error=0):
+        ansTable = [] 
+        ansTable.append(["i", "xi", "g(xi)", "f(xi)", "E"])
 
-        print("iter", "xi", "g(xi)", "f(xi)", "E")
-
-        fx = function(x0)
+        x = sp.symbols('x')
+        function = pe(f)
+        g_function = pe(g)
+        fx = function.subs(x,x0)
 
         if fx == 0:
             print(f"{x0} is the root")
@@ -19,19 +25,18 @@ class FixedPoint:
         if iter < 1:
             print("The iterator value is wrong")
             return "The iterator value is wrong"
-        if tolerancia < 0:
+        if tol < 0:
             print("Tolerance error, must be greater than or equal to 0")
             return "Tolerance error, must be greater than or equal to 0"
 
-        self.values.append([0, str(x0), str("{:.2e}".format(fx)), None])
-        contador = 0
-        error = tolerancia + 0.1
-        while fx != 0 and error > tolerancia and contador < iter:
-            xn = g_function(x0)
-            fi = function(xn)
-            fn = function(x0)
-            print(contador, x0, xn, fn, error)
-
+        self.values.append([0, str(x0), str(fx), None])
+        counter = 0
+        error = tol + 0.1
+        while fx != 0 and error > tol and counter < iter:
+            xn = g_function.subs(x,x0)
+            fi = function.subs(x,xn)
+            fn = function.subs(x,x0)
+            ansTable.append([counter, "{0:0.9e}".format(x0), "{0:0.9e}".format(xn), "{0:0.9e}".format(fn), "{0:0.9e}".format(error)])
             if type_error == 0:
                 error = abs(xn-x0)
             else:
@@ -39,22 +44,20 @@ class FixedPoint:
 
             x0 = xn
 
-            contador = contador + 1
+            counter = counter + 1
 
-            self.values.append([contador, str(xn), str(
-                "{:.2e}".format(fi)), str("{:.2e}".format(error))])
-
+            self.values.append([counter, str(xn), str(fi), str(error)])
+        
 
         if fx == 0:
-            return f"{x0} is the root"
-        elif error < tolerancia:
-            return f"{x0} is an aproximation with tolerance of {tolerancia} and after {contador} iterations"
+            ansTable.append(x0)
+            return ansTable
+        elif error < tol:
+            #ansTable.append([x0,tol,counter])
+            return ansTable , f'The root is an approximation of: {x0} with a tolerance of {tol}'
         else:
             return f"Failed after {iter} iterations "
 
     def tabla_values(self):
         print(self.values)
         return self.values
-
-fpoint = FixedPoint()
-print(fpoint.evaluate(-0.5, 1E-7, 100, fc.f, fc.e))
--- src/numericalapp/Function_Roots/views.py
+++ src/numericalapp/Function_Roots/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .Forms import *
+from .Methods import FixedPoint
+#  x0, tolerancia, iter, function, g_function, type_error=0
 
 def fixedPoint_view(request, *args, **kwargs):
-    return render(request, 'methods/function_roots/fixedPoint.html',{})
+    if request.method == 'POST':
+        form = FixedPointForm(request.POST)
+        fp = FixedPoint.FixedPoint()
+        x0 = float(form.data['x0'])
+        Tol = float(form.data['Tol'])
+        Iter = int(form.data['Iter'])
+        F = form.data['F']
+        G = form.data['G']
+
+
+        res,sol = fp.evaluate(x0,Tol,Iter,F,G)
+        print(res)
+        return render(request, 'methods/function_roots/fixedPoint.html',{'form':form,'res':res,'sol':sol})
+    else:
+        form = FixedPointForm()
+        return render(request, 'methods/function_roots/fixedPoint.html',{'form':form})
 
 def secant_view(request, *args, **kwargs):
     return render(request, 'methods/function_roots/secant.html',{})
