d63e740e58b0a67c8f8ab0cd48e45064359669a3
Sanchezt99 <msanchezt@eafit.edu.co>
2020-11-22 19:24:25 -0500

    Interpolation

--- src/numericalapp/Function_Roots/views.py
+++ src/numericalapp/Function_Roots/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def fixedPoint_view(request, *args, **kwargs):
-    return render(request, 'methods/function_roots/fixedPoint.html',{})
+    return render(request, 'methods/function_roots/fixedPoint.html', {})
 
 def secant_view(request, *args, **kwargs):
-    return render(request, 'methods/function_roots/secant.html',{})
+    return render(request, 'methods/function_roots/secant.html', {})
 
 def newton_view(request, *args, **kwargs):
-    return render(request, 'methods/function_roots/newton.html',{})
+    return render(request, 'methods/function_roots/newton.html', {})
