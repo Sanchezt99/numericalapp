494f3c41b513f7518787690c134da12e5732d385
Maira Cristina Castrillon Montoya <mccastrillon@intranet.nexos.com.co>
2020-11-24 10:54:47 -0500

    added url, templates, views, modules1,2,3,19,20,21,E8

--- src/numericalapp/numericalapp/urls.py
+++ src/numericalapp/numericalapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from pages.views import home_view, methods_view, about_view
+from pages.views import home_view, methods_view, about_view, help_view
 from Equation_Systems.views import doolittle_view, jacobi_view, gaussSeidel_view, gaussSimple_view
 from Function_Roots.views import fixedPoint_view, newton_view, secant_view, incrementalsearch_view,falseposition_view,bisection_view
 from Interpolation.views import splains_view, vandermonde_view, newtondivdif_view, lagrange_view, neville_view
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", home_view, name = "index.index"),
     path("methods/", methods_view, name="methods.index"),
     path("about/", about_view, name="about.index"),
+    path("help/", help_view, name="help.index"),
     path("fixedPoint/", fixedPoint_view, name="fixedPoint.index"),
     path("secant/", secant_view, name="secant.index"),
     path("newton/", newton_view, name="newton.index"),
