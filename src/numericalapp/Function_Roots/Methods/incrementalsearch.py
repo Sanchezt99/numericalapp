40bfae8e24f60fda3336b46e82294f43e1df6be6
root <root@LAPTOP-J03EB5V8.localdomain>
2020-11-25 00:47:24 -0500

    incremental searches

--- src/numericalapp/Function_Roots/Methods/incrementalsearch.py
+++ src/numericalapp/Function_Roots/Methods/incrementalsearch.py
@@ -1,68 +1,57 @@
-import math
-import sympy as sp
-from sympy.parsing.sympy_parser import parse_expr as pe
+
 from math import *
 
-class incrementalsearch:
-    def __init__(self):
-        self.values = []
 
-    def evaluate(self, tol, xi, niter, fun, dfun, type_error=0):
-        
-        ansTable = [] 
-        ansTable.append(["i", "xi", "f(xi)", "E"])
-        x = sp.symbols('x')
-        function = pe(fun)
-        dfunction = pe(dfun)
-        fx = function.subs(x,xi)
-        dfx = dfunction.subs(x,xi) 
+def Incrementalsearches(function,initialvalue,delta,iterations):
+   print(function,initialvalue,delta,iterations)
+   f=function
+   xo=initialvalue     
+   d=delta
+   i=iterations
+   message = ""
+   ansTable = []
 
-        if fx == 0:
-            return ansTable, str(fx) + " is a root."
-        if dfun == 0:
-            return "The derivate can't be 0"
+   def func():
+       x=xo
+       res = eval(f)
+       return float(res)
+      
+   def fun1():
+       x=xa
+       res = eval(f)
+       return res
 
-        counter = 0
-        error = tol + 1
-
-        self.values.append([counter, str(xi), str(
-            "{:.2e}".format(fx)), str(dfx), None])
+   if func() == 0.0:
+       message = str(x) + " is root"
+       return message, ansTable
 
 
-        xn = 0
-        while error > tol and fx != 0 and dfx != 0 and counter - 1 < niter:
-            xn = xi - (fx.subs(x,xi)/dfx.subs(x,xi))
-            fx = function.subs(x,xn)
-            dfx = dfunction.subs(x,xn)
-            ansTable.append([counter, "{0:0.9e}".format(xi), "{0:0.9e}".format(fx), "{0:0.2e}".format(error)])
-            #print(counter, xi, fun(xi),error)
+   else:
+       if message:
+           print("message")
+           return message
 
-            if type_error == 0:
-                error = abs(xn-xi)
-            else:
-                error = abs((xn-xi)/xn)
-
-            xi = xn
-
-            counter = counter + 1
-            #self.values.append([counter, str(xn), str("{:.2e}".format(fx)), str(dfx), str("{:.2e}".format(error))])
-        ansTable.append([counter, "{0:0.9e}".format(xi), "{0:0.9e}".format(fx), "{0:0.2e}".format(error)])
-        if fx == 0:
-            ansTable.append(xi)
-            #return f"{xi} is a root"
-            return ansTable , xi
-            
-        elif error < tol:
-            #return f"{xi} its an aproximation to a root with a tolerance of {tol}"
-            return ansTable , f'The root is an approximation of {xi} with a tolerance of {tol}'
-        elif dfx == 0:
-            return ansTable, f"{xi} Possible multiple root"
-        else:
-            return ansTable, f"Failed after {niter} iterations"
-
-    def tabla_values(self):
-        return self.values
-
-# inc = incrementalsearch()
-#print(inc.evaluate(0.00001,1,100,fc.fPrime,fc.fPrimePrime))
+       cont = 1 
+       encontrado=False
+       while (cont < i):
+           res1 = func()
+           xa = xo + d
+           res2 = fun1()
+           if res2 == 0:
+               message = str(xa) + " is root"
+               return message, []
+               print (xa,"is root")
+           else:
+               if (res1*res2 < 0):
+                   m = "There is a root of f in [","{:14.10f}".format(xo),",","{:14.10f}".format(xa),"]"
+                   ansTable.append(m)
+                   encontrado=True
+           cont = cont + 1
+           xo=xa
+   if ((cont==i) and (not encontrado)):
+       message = "With the number of requested iterations, no interval was found that could contain a root"
+       print("With the number of requested iterations, no interval was found that could contain a root")
+       
+       return message, ansTable
+Incrementalsearches('log(sin(x)**2+1)-1/2', -3, 0.5, 100)
 
