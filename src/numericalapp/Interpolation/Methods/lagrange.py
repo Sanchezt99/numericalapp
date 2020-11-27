25850825ed0c0bafd231b4623a451295fa3e8670
Sanchezt99 <msanchezt@eafit.edu.co>
2020-11-27 18:23:05 -0500

    Merge

--- src/numericalapp/Interpolation/Methods/lagrange.py
+++ src/numericalapp/Interpolation/Methods/lagrange.py
@@ -1,30 +1,30 @@
 import sympy as sp
-from sympy import *
+from sympy.parsing.sympy_parser import parse_expr as pe
 
 
 def lagrange(x,y):        
     n=len(x)
     print(n)
-    xx = symbols('x')
     print("Lagrange interpolating polynomials:")
+    lk = []
     for i in range(len(x)):    
         print(i)
-        l=1    
+        l=''    
+        denominator = 1
         for j in range(len(y)):
             if i!=j :
-                l=l*((xx-x[j])/(x[i]-x[j]))
-        ll = expand(l)
-        print("L"+str(i), ll)    
+                denominator *= pe(f'1/({x[i]}-{-x[j]})')
+                sign = '+' if -x[i] > 0 else '-'
+                number = abs(x[j])
+                l=f'{l}*(x{sign}{number})' if number != 0 else f'{l}*(x)'
+        print(denominator,l)
     print("Lagrange polynom")
     for i in range(len(x)): 
         print(i)
         print(str(y[i])+"*L"+str(i),end='')
         if i<len(x)-1:
-             print("+",end='')
-             
+            print("+",end='')
 
 x=[-1,0,3,4]
 y=[15.5,3,8,1]
 lagrange(x,y)
-
-#x = list(x.split(" ,")) 
