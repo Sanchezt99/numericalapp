7c0960b49061ada6840a65ca84da07f0f2ee1a53
Sanchezt99 <msanchezt@eafit.edu.co>
2020-11-27 20:36:59 -0500

    Lagrange done

--- src/numericalapp/Interpolation/Methods/lagrange.py
+++ src/numericalapp/Interpolation/Methods/lagrange.py
@@ -22,8 +22,13 @@ def lagrange(x,y):
                 sign = '+' if -x[i] > 0 else '-'
                 number = abs(x[j])
                 l=f'{l}*(x{sign}{number})' if number != 0 else f'{l}*(x)'
-        lps.append(str(denominator) + l)
+        lps.append(str('{:.5f}'.format(denominator)) + l)
     
     for i in range(len(x)): 
         lp.append(f'{y[i]}*({lps[i]})')
     return lps, lp, message
+
+x = [1,2,3,4]
+y = [5,6,7,8]
+
+lagrange(x,y)
